c263e6260380aa6f7f4db6c41fccc88afdbcc7cd
Wim Pouw <wimpouw@gmail.com>
2026-07-09 11:05:52 +0200

    upload

--- LandPageQuarto/RpackagesInstall.R
+++
@@ -1,4 +0,0 @@
-# install.R
-pkgs <- c("tidyverse", "phonfieldwork", "xml2", "reticulate", "knitr", "rmarkdown")
-missing <- pkgs[!pkgs %in% rownames(installed.packages())]
-if (length(missing)) install.packages(missing, repos = "https://cloud.r-project.org")
